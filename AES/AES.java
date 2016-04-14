a083addbf32694562f5c233bdc52e4102d0dfb81
EdDT-Phi <EdDT.Phi@gmail.com>
2016-04-14 00:11:38 -0500

    Encode and Decode working and writing from/to files

--- AES/AES.java
+++ AES/AES.java
@@ -1,10 +1,12 @@
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 class AES {
 
-   static int[][] byteSubs = 
-   {
+   private static final boolean DEBUG = false;
+
+   final static int[][] byteSubs = {
       {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76},
       {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0},
       {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15},
@@ -20,11 +22,9 @@ class AES {
       {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A},
       {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
       {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
-      {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
-   };
+      {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}};
 
-    static int[][] invByteSubs = 
-   {
+   final static int[][] invByteSubs = {
       {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},
       {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},
       {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},
@@ -40,23 +40,89 @@ class AES {
       {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},
       {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},
       {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},
-      {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}
-    };
-    
-   static int[][] rcon = {
-   {0x01, 0, 0, 0},{0x02, 0, 0, 0},{0x04, 0, 0, 0},{0x08, 0, 0, 0},{0x10, 0, 0, 0},
-   {0x20, 0, 0, 0},{0x40, 0, 0, 0},{0x80, 0, 0, 0},{0x1b, 0, 0, 0},{0x36, 0, 0, 0},
-   {0x6c, 0, 0, 0},{0xd8, 0, 0, 0},{0xab, 0, 0, 0},{0x4d, 0, 0, 0},{0x9a, 0, 0, 0}};
+      {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}};
+
+   final static int[] LogTable = {
+      0,   0,  25,   1,  50,   2,  26, 198,  75, 199,  27, 104,  51, 238, 223,   3,
+      100,   4, 224,  14,  52, 141, 129, 239,  76, 113,   8, 200, 248, 105,  28, 193,
+      125, 194,  29, 181, 249, 185,  39, 106,  77, 228, 166, 114, 154, 201,   9, 120,
+      101,  47, 138,   5,  33,  15, 225,  36,  18, 240, 130,  69,  53, 147, 218, 142,
+      150, 143, 219, 189,  54, 208, 206, 148,  19,  92, 210, 241, 64,  70, 131,  56,
+      102, 221, 253,  48, 191,   6, 139,  98, 179,  37, 226, 152,  34, 136, 145,  16,
+      126, 110,  72, 195, 163, 182,  30,  66,  58, 107,  40,  84, 250, 133,  61, 186,
+      43, 121,  10,  21, 155, 159,  94, 202,  78, 212, 172, 229, 243, 115, 167,  87,
+      175,  88, 168,  80, 244, 234, 214, 116,  79, 174, 233, 213, 231, 230, 173, 232,
+      44, 215, 117, 122, 235,  22,  11, 245,  89, 203,  95, 176, 156, 169,  81, 160,
+      127,  12, 246, 111,  23, 196,  73, 236, 216,  67,  31,  45, 164, 118, 123, 183,
+      204, 187,  62,  90, 251,  96, 177, 134,  59,  82, 161, 108, 170,  85,  41, 157,
+      151, 178, 135, 144,  97, 190, 220, 252, 188, 149, 207, 205, 55,  63,  91, 209,
+      83,  57, 132,  60,  65, 162, 109,  71,  20,  42, 158,  93,  86, 242, 211, 171,
+      68,  17, 146, 217,  35,  32,  46, 137, 180, 124, 184,  38, 119, 153, 227, 165,
+      103,  74, 237, 222, 197,  49, 254,  24,  13,  99, 140, 128, 192, 247, 112,   7};
+
+   final static int[] AlogTable = {
+      1,   3,   5,  15,  17,  51,  85, 255,  26,  46, 114, 150, 161, 248,  19,  53,
+      95, 225,  56,  72, 216, 115, 149, 164, 247,   2,   6,  10,  30, 34, 102, 170,
+      229,  52,  92, 228,  55,  89, 235,  38, 106, 190, 217, 112, 144, 171, 230,  49,
+      83, 245,   4,  12,  20,  60,  68, 204,  79, 209, 104, 184, 211, 110, 178, 205,
+      76, 212, 103, 169, 224,  59,  77, 215,  98, 166, 241,   8,  24, 40, 120, 136,
+      131, 158, 185, 208, 107, 189, 220, 127, 129, 152, 179, 206,  73, 219, 118, 154,
+      181, 196,  87, 249,  16,  48,  80, 240,  11,  29,  39, 105, 187, 214,  97, 163,
+      254,  25,  43, 125, 135, 146, 173, 236,  47, 113, 147, 174, 233,  32,  96, 160,
+      251,  22,  58,  78, 210, 109, 183, 194,  93, 231,  50,  86, 250,  21,  63,  65,
+      195,  94, 226,  61,  71, 201,  64, 192,  91, 237,  44, 116, 156, 191, 218, 117,
+      159, 186, 213, 100, 172, 239,  42, 126, 130, 157, 188, 223, 122, 142, 137, 128,
+      155, 182, 193,  88, 232,  35, 101, 175, 234,  37, 111, 177, 200,  67, 197,  84,
+      252,  31,  33,  99, 165, 244,   7,   9,  27,  45, 119, 153, 176, 203,  70, 202,
+      69, 207,  74, 222, 121, 139, 134, 145, 168, 227,  62,  66, 198, 81, 243,  14,
+      18,  54,  90, 238,  41, 123, 141, 140, 143, 138, 133, 148, 167, 242,  13,  23,
+      57,  75, 221, 124, 132, 151, 162, 253,  28,  36, 108, 180, 199, 82, 246,   1};
+
+   final static int[][] rcon = {
+      {0x01, 0, 0, 0},{0x02, 0, 0, 0},{0x04, 0, 0, 0},{0x08, 0, 0, 0},{0x10, 0, 0, 0},
+      {0x20, 0, 0, 0},{0x40, 0, 0, 0},{0x80, 0, 0, 0},{0x1b, 0, 0, 0},{0x36, 0, 0, 0},
+      {0x6c, 0, 0, 0},{0xd8, 0, 0, 0},{0xab, 0, 0, 0},{0x4d, 0, 0, 0},{0x9a, 0, 0, 0}};
+
+
    public static void main(String[] args) {
       if(args.length != 3) {
-         System.out.println("Usage: java AES option keyFile inputFile");
+         debug("Usage: java AES option keyFile inputFile");
       }
 
-      // String stringKey     =  "FFEEDDCCBBAA00998877665544332211FFEEDDCCBBAA00998877665544332211";
-      String stringKey  =  "0000000000000000000000000000000000000000000000000000000000000000";
-      String input      =  "00112233445566778899AABBCCDDEEFF";
+      boolean encode;
+      String stringKey;
+      Scanner inputFile;
+      String outputFileName = args[2];
 
-      // System.out.println("key length: " + key.length() + "\n");
+
+      if (args[0].toLowerCase().equals("e")) {
+         encode = true;
+         outputFileName += ".enc";
+      } else if (args[0].toLowerCase().equals("d")){
+         encode = false;
+         outputFileName += ".dec";
+      } else {
+         debug("option must be one of \"e\" or \"d\"");
+         return;
+      }
+
+      try{
+         Scanner keyFile = new Scanner(new File(args[1]));
+         stringKey = keyFile.next();
+         keyFile.close();
+      } catch (Exception e) {
+         System.out.printf("%s is an invalid key file\n", args[1]);
+         return;
+      }
+
+      try{
+         inputFile = new Scanner(new File(args[2]));
+      } catch (Exception e) {
+         System.out.printf("%s is an invalid input file\n", args[2]);
+         return;
+      }
+
+      // debug("key length: " + key.length() + "\n");
 
       int[][] key = new int[60][4];
       int[][] block = new int[4][4];
@@ -65,23 +131,61 @@ class AES {
          key[i/4][i%4] = Integer.decode("0x" + stringKey.substring(i*2 , i*2 + 2).toLowerCase());
       }
 
+      debug("CipherKey:");
+      printArr(key, 8, " ", false);
 
-      for(int i = 0; i < input.length()/2; ++i) {
-         block[i/4][i%4] = Integer.decode("0x" + input.substring(i*2, i*2 + 2).toLowerCase());
-      }
-      System.out.println("Plaintext:");
-      printArr(block, 4);
-
+      debug("Expansion:");
       keyExpansion(key);
+      printArr(key, 32, "", false);
 
-      System.out.println("\nCipherKey:");
-      printArr(key, 8);
+      String input;
+      try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outputFileName)))) {
+         while(inputFile.hasNext()) {
+            input = inputFile.next();
+            for(int i = 0; i < 16; ++i) {
+               block[i/4][i%4] = i < input.length()/2 ?  Integer.decode("0x" + input.substring(i*2, i*2 + 2).toLowerCase()) : 0;
+            }
 
-      System.out.println("\nExpansion:");
-      printArr(key, 32);
+            if (encode) {
+               debug("Plaintext:");
+               printArr(block, 4, " ", false);
 
+               debug("Ciphertext:");
+               encode(key, block);
+               printArr(block, 4, " ", false);
 
-      encode(key, block);
+            } else {
+               debug("Ciphertext:");
+               printArr(block, 4, " ", false);
+
+               decode(key, block);
+               debug("Decryption of Ciphertext:");
+               printArr(block, 4, " ", false);
+               debug("\nDecryption of Ciphertext:");
+            }
+
+            bw.append(printAsLine(block) + "\n");
+         }
+      } catch (Exception ignore){
+         System.out.println("Something went wrong");
+      }
+      inputFile.close();
+   }
+
+   public static void decode(int[][] key, int[][] block) {
+      int round = 14;
+      addRoundKey(block, round--, key);
+      invShiftRows(block);
+      invSubBytes(block);
+
+      for(int i = 0; i < 13; i++) {
+         addRoundKey(block, round--, key);
+         invMixColumns(block);
+         invShiftRows(block);
+         invSubBytes(block);
+      }
+
+      addRoundKey(block, round--, key);
    }
 
    public static void encode(int[][] key, int[][] block) {
@@ -91,67 +195,21 @@ class AES {
       for(int i = 0; i < 13; i++) {
          for(int[] row: block)
             subBytes(row);
-         System.out.printf("After subBytes:\n");
+
+         debug("After subBytes:");
          printAsLine(block);
 
-
-
          shiftRows(block);
-         
-         // MIX COLUMNS
-
+         mixColumns(block);
          addRoundKey(block, round++, key);
       }
 
       for(int[] row: block)
             subBytes(row);
-         
+
       shiftRows(block);
       addRoundKey(block, round++, key);
    }
-
-   public static void addRoundKey(int[][] block, int start, int[][] key) {
-      for(int i = 0; i < 4; i++) {
-         XOR(block[1], key[start*4 + i]);
-      }
-      System.out.printf("After addRoundKey(%d):\n", start);
-      printAsLine(block);
-   }
-
-   public static void printAsLine(int[][] block) {
-      for(int[] row: block) {
-         for(int i: row) {
-            System.out.printf("%x", i);
-         }
-      }
-      System.out.println();
-   }
-
-   public static void shiftRows(int[][] block) {
-      int temp = block[0][1];
-      block[0][1] = block[1][1];
-      block[1][1] = block[2][1];
-      block[2][1] = block[3][1];
-      block[3][1] = temp;
-
-      temp = block[0][2];
-      block[0][2] = block[2][2];
-      block[2][2] = temp;
-
-      temp = block[1][2];
-      block[1][2] = block[3][2];
-      block[3][2] = temp;
-
-      temp = block[0][3];
-      block[0][3] = block[3][3];
-      block[3][3] = block[2][3];
-      block[2][3] = block[1][3];
-      block[1][3] = temp;
-
-      System.out.printf("After shiftRows:\n");
-      printAsLine(block);
-   }
-
 
    public static void keyExpansion(int[][] key){
       int val = 0;
@@ -175,31 +233,12 @@ class AES {
       }
    }
 
-   public static void subBytes(int[] word) {
+   public static void addRoundKey(int[][] block, int start, int[][] key) {
       for(int i = 0; i < 4; i++) {
-         word[i] = byteSubs[word[i] >> 4][word[i] & 0xF];
-      } 
-   }
-
-   public static void XOR(int[] word1, int[] word2) {
-      for(int i = 0; i < 4; i++) {
-         word1[i] ^= word2[i];
-      } 
-   }
-
-   public static void printArr(int[][] arr, int n) {
-      for(int i =0; i < 4; i++) {
-         for(int j = 0; j < n; ++j) {
-            if(j != 0 && j % 4 == 0)
-               System.out.print(" ");
-            if(arr[j][i] < 16) {
-               System.out.printf("0%x", arr[j][i]);
-            } else {
-               System.out.printf("%x", arr[j][i]);
-            }
-         }
-         System.out.println();
+         XOR(block[i], key[start*4 + i]);
       }
+      debug("After addRoundKey("+start+"):");
+      printAsLine(block);
    }
 
    public static int[] rotate(int[] word, int i) {
@@ -209,28 +248,170 @@ class AES {
       copy[2] = word[(2 + i)%4];
       copy[3] = word[(3 + i)%4];
       return copy;
-      // return new {i[1], i[2], i[3], i[0]};
    }
 
-   // // http://www.cs.utsa.edu/~wagner/laws/AESkeys.html
-   // void KeyExpansion(byte[] key, word[] w, int Nw) {
-   //    int Nr = Nk + 6;
-   //    w = new byte[4*Nb*(Nr+1)];
-   //    int temp;
-   //    int i = 0;
-   //    while ( i < Nk) {
-   //       w[i] = word(key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]);
-   //       i++;
-   //    }
-   //    i = Nk;
-   //    while(i < Nb*(Nr+1)) {
-   //       temp = w[i-1];
-   //       if (i % Nk == 0)
-   //          temp = SubWord(RotWord(temp)) ^ Rcon[i/Nk];
-   //       else if (Nk > 6 && (i%Nk) == 4)
-   //          temp = SubWord(temp);
-   //       w[i] = w[i-Nk] ^ temp;
-   //       i++;
-   //    }
-   // }
+   public static void invShiftRows(int[][] block) {
+      int temp = block[0][1];
+      block[0][1] = block[3][1];
+      block[3][1] = block[2][1];
+      block[2][1] = block[1][1];
+      block[1][1] = temp;
+
+      temp = block[0][2];
+      block[0][2] = block[2][2];
+      block[2][2] = temp;
+
+      temp = block[1][2];
+      block[1][2] = block[3][2];
+      block[3][2] = temp;
+
+      temp = block[0][3];
+      block[0][3] = block[1][3];
+      block[1][3] = block[2][3];
+      block[2][3] = block[3][3];
+      block[3][3] = temp;
+
+      debug("After invShiftRows:");
+      printAsLine(block);
+   }
+
+   public static void shiftRows(int[][] block) {
+      int temp = block[0][1];
+      block[0][1] = block[1][1];
+      block[1][1] = block[2][1];
+      block[2][1] = block[3][1];
+      block[3][1] = temp;
+
+      temp = block[0][2];
+      block[0][2] = block[2][2];
+      block[2][2] = temp;
+
+      temp = block[1][2];
+      block[1][2] = block[3][2];
+      block[3][2] = temp;
+
+      temp = block[0][3];
+      block[0][3] = block[3][3];
+      block[3][3] = block[2][3];
+      block[2][3] = block[1][3];
+      block[1][3] = temp;
+
+      debug("After shiftRows:");
+      printAsLine(block);
+   }
+
+   public static void invSubBytes(int[][] block) {
+      for(int i = 0; i < 4; i++) {
+         for (int j = 0; j < 4; j ++) {
+            block[i][j] = invByteSubs[block[i][j] >> 4][block[i][j] & 0xF];
+         }
+      }
+
+      debug("After subBytes:");
+      printAsLine(block);
+   }
+
+   public static void subBytes(int[] word) {
+      for(int i = 0; i < 4; i++) {
+         // System.out.printf("B: %x\n", word[i]);
+         word[i] = byteSubs[word[i] >> 4][word[i] & 0xF];
+         // System.out.printf("A: %x\n", word[i]);
+      }
+   }
+
+   public static void XOR(int[] word1, int[] word2) {
+      for(int i = 0; i < 4; i++) {
+         word1[i] ^= word2[i];
+      }
+   }
+
+   private static int mul (int a, int b) {
+      int inda = (a < 0) ? (a + 256) : a;
+      int indb = (b < 0) ? (b + 256) : b;
+
+      if ( (a != 0) && (b != 0) ) {
+         int index = LogTable[inda] + LogTable[indb];
+         int val = AlogTable[ index % 255 ];
+         return val;
+      } else {
+          return 0;
+      }
+   }
+
+   // In the following two methods, the input c is the column number in
+   // your evolving state matrix st (which originally contained
+   // the plaintext input but is being modified).  Notice that the state here is defined as an
+   // array of bytes.  If your state is an array of integers, you'll have
+   // to make adjustments.
+   public static void mixColumns (int[][] block) {
+   // This is another alternate version of mixColumn, using the
+   // logtables to do the computation.
+
+      for(int i = 0; i < 4; i++) {
+         int a[] = new int[4];
+
+         // note that a is just a copy of st[.][c]
+         for (int j = 0; j < 4; j++)
+             a[j] = block[i][j];
+
+         // This is exactly the same as mixColumns1, if
+         // the mul columns somehow match the b columns there.
+         block[i][0] = mul(2,a[0]) ^ a[2] ^ a[3] ^ mul(3,a[1]);
+         block[i][1] = mul(2,a[1]) ^ a[3] ^ a[0] ^ mul(3,a[2]);
+         block[i][2] = mul(2,a[2]) ^ a[0] ^ a[1] ^ mul(3,a[3]);
+         block[i][3] = mul(2,a[3]) ^ a[1] ^ a[2] ^ mul(3,a[0]);
+      }
+      debug("After mixColumns:");
+      printAsLine(block);
+   } // mixColumn2
+
+   public static void invMixColumns (int[][] block) {
+      for(int i = 0; i < 4; i++) {
+         int a[] = new int[4];
+
+         // note that a is just a copy of st[.][c]
+         for (int j = 0; j < 4; j++)
+             a[j] = block[i][j];
+
+         block[i][0] = mul(0xE,a[0]) ^ mul(0xB,a[1]) ^ mul(0xD, a[2]) ^ mul(0x9,a[3]);
+         block[i][1] = mul(0xE,a[1]) ^ mul(0xB,a[2]) ^ mul(0xD, a[3]) ^ mul(0x9,a[0]);
+         block[i][2] = mul(0xE,a[2]) ^ mul(0xB,a[3]) ^ mul(0xD, a[0]) ^ mul(0x9,a[1]);
+         block[i][3] = mul(0xE,a[3]) ^ mul(0xB,a[0]) ^ mul(0xD, a[1]) ^ mul(0x9,a[2]);
+      }
+   } // invMixColumn2
+
+   public static String printAsLine(int[][] block) {
+      StringBuilder out = new StringBuilder();
+      for(int[] row: block) {
+         for(int i: row) {
+
+            out.append((i<16 ? "0":"") + Integer.toHexString(i).toUpperCase());
+            // System.out.printf("%x", i);
+         }
+      }
+      debug(out.toString());
+      return out.toString();
+   }
+
+   public static void printArr(int[][] arr, int n, String delim, boolean force) {
+      if (!force && !DEBUG) return;
+      for(int i =0; i < 4; i++) {
+         for(int j = 0; j < n; ++j) {
+            if(j != 0 && j % 4 == 0)
+               System.out.print(" ");
+            if(arr[j][i] < 16) {
+               System.out.printf("0%x%s", arr[j][i], delim);
+            } else {
+               System.out.printf("%x%s", arr[j][i], delim);
+            }
+         }
+         System.out.println();
+      }
+      System.out.println();
+   }
+
+   public static void debug(String s) {
+      if (DEBUG)
+         System.out.println(s);
+   }
 }
